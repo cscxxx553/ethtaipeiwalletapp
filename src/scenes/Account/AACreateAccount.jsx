9a2000ee8b10c894deaaf2bc43247957a6aa018f
Garrick H.C. Chen <garrickchen@gmail.com>
2023-04-21 20:33:41 +0800

    2023/4/21 update AA

--- src/scenes/Account/AACreateAccount.jsx
+++ src/scenes/Account/AACreateAccount.jsx
@@ -1,22 +1,25 @@
 import React, { useEffect, useState, useCallback } from 'react';
 import { EthAddressGenerator, RecoverAccount } from '../../utils/AccountUtils';
 import AccountDetail from './AccountDetail';
+import { AAWalletUtils } from '../../utils/AAWalletUtils';
 
 const recoveryPhraseKeyName = 'recoveryPhrase';
 
-function CreateAccount() {
+function AACreateAccount() {
 
   // Declare a new state variable, which we'll call "seedphrase"
-  const [seedphrase, setSeedphrase] = useState('');
+  // const [seedphrase, setSeedphrase] = useState('');
 
-  const [recoverSeedphrase, setRecoverSeedphrase] = useState('');
+  // const [recoverSeedphrase, setRecoverSeedphrase] = useState('');
+
+  const [tt, setTt] = useState();
 
   // Declare a new state variable, which we'll call "account"
-  const [account, setAccount] = useState(null);
+  const [aaAccount, setAaAccount] = useState(null);
 
   // Declare a new state variable, which we'll call "showRecoverInput"
   // and initialize it to false
-  const [showRecoverInput, setShowRecoverInput] = useState(false);
+  const [aashowRecoverInput, setAaShowRecoverInput] = useState(false);
     
   function createAccount() {
     // Your create account logic here
@@ -24,14 +27,14 @@ function CreateAccount() {
     const result = EthAddressGenerator();
     console.log(result.account)
     console.log(result.seedPhrase)
-    setAccount(result.account)
-    setSeedphrase(result.seedPhrase)
+    setAaAccount(result.account)
+    // setSeedphrase(result.seedPhrase)
   }
 
   function handleChange(event) {
     // Update the seedphrase state with the value from the text input
-    setSeedphrase(event.target.value);
-    setRecoverSeedphrase(event.target.value);
+    // setSeedphrase(event.target.value);
+    // setRecoverSeedphrase(event.target.value);
   }
 
   const handleKeyDown = async (event) => {
@@ -44,30 +47,34 @@ function CreateAccount() {
   const recoverAccount = useCallback(
     // recoverAccount could be used without recoveryPhrase as an arguement but then we would have to
     // put it in a deps array.
-    async (recoveryPhrase) => {
+    // async (recoveryPhrase) => {
 
-      // Call the RecoverAccount function with no arguments
-      // Call the RecoverAccount function and pass it 0 and the current seedphrase
-      const result = await RecoverAccount(recoveryPhrase);
+    //   // Call the RecoverAccount function with no arguments
+    //   // Call the RecoverAccount function and pass it 0 and the current seedphrase
+    //   const result = await RecoverAccount(recoveryPhrase);
 
-      // Update the account state with the newly recovered account
-      setAccount(result.account);
+    //   // Update the account state with the newly recovered account
+    //   setAccount(result.account);
 
-      if (localStorage.getItem(recoveryPhraseKeyName) !== recoveryPhrase) {
-        localStorage.setItem(recoveryPhraseKeyName, recoveryPhrase);
-      }
+    //   if (localStorage.getItem(recoveryPhraseKeyName) !== recoveryPhrase) {
+    //     localStorage.setItem(recoveryPhraseKeyName, recoveryPhrase);
+    //   }
 
-    }, []
+    // }, []
   );
 
   useEffect(() => {
-
-    const localStorageRecoveryPhrase = localStorage.getItem(recoveryPhraseKeyName)
-    if (localStorageRecoveryPhrase) {
-      setSeedphrase(localStorageRecoveryPhrase);
-      recoverAccount(localStorageRecoveryPhrase);
-    }
-  }, [recoverAccount])
+    // setTt();
+    AAWalletUtils().then( function (res) {
+      console.log("AACreateAccount print AAWalletUtils::",res);
+      setTt(res);
+    });
+    // const localStorageRecoveryPhrase = localStorage.getItem(recoveryPhraseKeyName)
+    // if (localStorageRecoveryPhrase) {
+    //   setSeedphrase(localStorageRecoveryPhrase);
+    //   recoverAccount(localStorageRecoveryPhrase);
+    // }
+  }, [])
 
 
   return (
@@ -80,14 +87,14 @@ function CreateAccount() {
         {/* Add a button to toggle showing the recover account input and button */}
         {/* If show recover input is visible, clicking the button again will submit the phrase in the input */}
         <button type="button" className="btn btn-outline-primary ml-3"
-          onClick={() => showRecoverInput ? recoverAccount(seedphrase) : setShowRecoverInput(true)}
+          onClick={() => aashowRecoverInput ? recoverAccount(seedphrase) : setAaShowRecoverInput(true)}
           // if the recoveryinput is showing but there is no seedphrase, disable the ability to recover account
-          disabled={showRecoverInput && !seedphrase}
+          disabled={aashowRecoverInput && !seedphrase}
         >
           Recover account
         </button>
         {/* Show the recover account input and button if showRecoverInput is true */}
-        {showRecoverInput && (
+        {aashowRecoverInput && (
           <div className="form-group mt-3">
             <input type="text" placeholder='Paste Seedphrase and Press Enter' className="form-control"
             value={recoverSeedphrase} onChange={handleChange} onKeyDown={handleKeyDown} />
@@ -95,17 +102,14 @@ function CreateAccount() {
           </div>
         )}
       </form>
-      <p>Address: {account === null? "": account.address}</p>
+      <p>Address: {tt === null? "": tt}</p>
       {/*<p>seedphrase: {seedphrase === null? "": seedphrase}</p>*/}
       <hr/>
-      {account &&
-        <>
-          <hr />
-          <AccountDetail account={account} />
-        </>
-      }
+      
+      
+      
     </div>
   );
 }
 
-export default CreateAccount;
+export default AACreateAccount;
